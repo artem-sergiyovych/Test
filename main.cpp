6e14663e120aeaa9487f35c37d0e4de85a8f9fcd
artem-sergiyovych <99665168+artem-sergiyovych@users.noreply.github.com>
2023-04-27 13:47:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,80 +3,94 @@
 #include <memory>
 #include <vector>
 
-class ElectronicDevice{
+class ElectronicDevice {
 protected:
     std::string brand;
     std::string model;
+
 public:
     ElectronicDevice(std::string brand, std::string model)
-                     :brand(brand), model(model){}
-    void display(){
-        std::cout << "Brand: " << brand << "\nModel: "<< model << std::endl;
-    }
+            : brand(brand), model(model) {}
+
+    virtual void display() = 0;
 };
 
-
-
-class Smartwatch : public ElectronicDevice{
-
-protected:
+class Smartwatch : public ElectronicDevice {
+private:
     bool hasGPS;
+
 public:
     Smartwatch(std::string brand, std::string model, bool hasGPS)
-        :ElectronicDevice(brand, model), hasGPS(hasGPS){}
-    void display(){
-        if (hasGPS){
-        std::cout << "Brand: " << brand << "\nModel: "<< model<< "\nHas GPS "<< std::endl;
-        }else std::cout << "Brand: " << brand << "\nModel: "<< model<< "\nDoesnt have GPS "<< std::endl;
+            : ElectronicDevice(brand, model), hasGPS(hasGPS) {}
+
+    void display() override {
+        std::cout << "Brand: " << brand << " Model: " << model;
+        if (hasGPS) {
+            std::cout << " Has GPS";
+        } else {
+            std::cout << " Doesn't have GPS";
+        }
+        std::cout << std::endl;
     }
 };
 
-class Smartphone : public ElectronicDevice{
-protected:
+class Smartphone : public ElectronicDevice {
+private:
     int storageCapacity;
+
 public:
     Smartphone(std::string brand, std::string model, int storageCapacity)
-        :ElectronicDevice(brand, model), storageCapacity(storageCapacity){}
-    void display(){
-        std::cout << "Brand: " << brand << "\nModel: "<< model<< "\nStorage capacity: "<< storageCapacity<< std::endl;
+            : ElectronicDevice(brand, model), storageCapacity(storageCapacity) {}
+
+    void display() override {
+        std::cout << "Brand: " << brand << " Model: " << model
+                  << " Storage Capacity: " << storageCapacity << " GB"
+                  << std::endl;
     }
 };
 
-class Laptop : public ElectronicDevice{
+class Laptop : public ElectronicDevice {
 protected:
     int ramSize;
+
 public:
     Laptop(std::string brand, std::string model, int ramSize)
-        :ElectronicDevice(brand, model), ramSize(ramSize){}
-    void display(){
-        ElectronicDevice::display();
-        std::cout <<"Ram memory: "<< ramSize<< std::endl;
+            : ElectronicDevice(brand, model), ramSize(ramSize) {}
+
+    void display() override {
+        std::cout << "Brand: " << brand << " Model: " << model
+                  << " RAM Size: " << ramSize << " GB" << std::endl;
     }
 };
-class GamingLaptop : public Laptop{
-protected:
+
+class GamingLaptop : public Laptop {
+private:
     std::string gpuModel;
+
 public:
-    GamingLaptop(std::string brand, std::string model, int ramSize, std::string gpuModel)
-        :Laptop(brand, model, ramSize), gpuModel(gpuModel){}
-    void display(){
-        Laptop::display();
-        std::cout << "Model of GPU" << gpuModel;
+    GamingLaptop(std::string brand, std::string model, int ramSize,
+                 std::string gpuModel)
+            : Laptop(brand, model, ramSize), gpuModel(gpuModel) {}
+
+    void display() override {
+        std::cout << "Brand: " << brand << " Model: " << model
+                  << " RAM Size: " << ramSize << " GB"
+                  << " GPU Model: " << gpuModel << std::endl;
     }
 };
 
-
-
-class Store{
+class Store {
 private:
     std::vector<std::unique_ptr<ElectronicDevice>> devices;
+
 public:
-    void addDevice(std::unique_ptr<ElectronicDevice> device ){
+    void addDevice(std::unique_ptr<ElectronicDevice> device) {
         devices.push_back(std::move(device));
     }
-    void displayDevices(){
+
+    void displayDevices() {
         std::cout << "Devices:\n";
-        for ( const auto& device : devices) {
+        for (const auto& device : devices) {
             device->display();
         }
     }
@@ -90,6 +104,11 @@ int main()
     auto laptop = std::make_unique<Laptop>("Huawei", "ThinkPad", 16);
     auto laptop2 = std::make_unique<Laptop>("Huawei", "WidePad", 24);
     auto gaminglaptop = std::make_unique<GamingLaptop>("Acer", "GigaPad", 16, "Nvidia 3080TI");
+    store.addDevice(std::move(smartwatch));
+    store.addDevice(std::move(smartphone));
+    store.addDevice(std::move(laptop));
+    store.addDevice(std::move(laptop2));
+    store.addDevice(std::move(gaminglaptop));
     store.displayDevices();
     return 0;
 }
